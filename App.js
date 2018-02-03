e90491da45895082ba787302c647565376f13237
Kalen Hammann <kalen7@gmail.com>
2018-02-03 12:53:06 -0500

    Working video player

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Video } from 'expo';
 
 export default class App extends React.Component {
   render() {
+    const { width } = Dimensions.get('window');
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>React Native Video</Text>
+        <Video
+        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        shouldPlay
+        resizeMode="cover"
+        style={{ width, height: 300 }}
+        />
       </View>
     );
   }
